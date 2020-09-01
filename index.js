c45c64ee5c29e757c4b7a9e0e48dc51084b572dc
Abhik <abhikshrestha@gmail.com>
2020-09-01 19:21:57 +1000

    added billing and client side

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 const keys = require('./config/keys');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cookieSession({
     maxAge:30*24*60*60*1000,
     keys:[keys.cookieKey]
@@ -17,6 +19,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'));
+    
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 mongoose.connect(keys.mongoURI);
 
