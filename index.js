77e7432120cc73f71f4a612b2bb62bcf81d810d8
Abhik <abhikshrestha@gmail.com>
2020-09-05 18:21:53 +1000

    Model update

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('app/client/build'));
