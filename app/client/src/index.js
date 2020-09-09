b0a81bc6379734ebf8618248ac9e980736a54e15
Abhik <abhikshrestha@gmail.com>
2020-09-09 20:55:13 +1000

    Big changes

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore,applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
-import 'materialize-css/dist/css/materialize.min.css';
-
+import 'materialize-css/dist/css/materialize.min.css'
 import App from './components/App';
 import reducers from './reducers';
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers,{},applyMiddleware(reduxThunk));
 
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const Survey = mongoose.model('Surveys');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
 
 module.exports = app =>{
     app.post('/api/surveys',requireLogin,requireCredits,(req,res)=>{
@@ -12,7 +15,9 @@ module.exports = app =>{
             body,
             recipients: recipients.split(',').map(email => ({ email: email.trim() })), 
             _user:req.user.id,
-            dateSent:date.now()
-        })
+            dateSent:Date.now()
+        });
+        const mailer = new Mailer(survey,surveyTemplate(survey));
+        mailer.send();
     });
 };
