80c7be18e5ada847632b7b2d2b80ba44a372628b
Abhik <abhikshrestha@gmail.com>
2020-09-19 15:24:54 +1000

    Cleaning up code

--- app/client/src/components/surveys/SurveyForm.js
+++ app/client/src/components/surveys/SurveyForm.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import {reduxForm,Field} from 'redux-form';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+    {label:'Survey Title', name:'title'},
+    {label:'Subject line',name:'subject'},
+    {label:'Email body',name:'body'},
+    {label:'Recipient list',name:'emails'}
+];
+
+
 class SurveyForm extends React.Component{
+
+    renderFields(){
+        
+    }
+
     render(){
         return(
             <div>
             <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-            <Field type="text" name="surveyTitle" component="input" />
+            {this.renderFields()};
             <button type="submit">Submit</button>
             </form>
             </div>
